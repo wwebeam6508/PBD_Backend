74918dcd5ddcf7f8c346773d421f83beabe4fdc2
Wim <policebeam@gmail.com>
2022-12-18 14:35:37 +0700

    move pageArray to helper.util.js

--- src/utils/helper.util.js
+++ src/utils/helper.util.js
@@ -9,7 +9,28 @@ function emptyOrRows (rows) {
   return rows
 }
 
+function pageArray (totalSize,pageSize,page, maxLength) {
+  const currentPage = Number(page)
+  const current_position = Math.floor(maxLength/2)
+  const totalPage = Math.ceil(totalSize/pageSize)
+
+  const startPoint = currentPage - current_position >= 1 ? currentPage - current_position : 1
+  const endPoint = currentPage + current_position <= totalPage ? currentPage + current_position : totalPage
+  let pages = []
+  if(startPoint !== 1){
+      pages.push("...")
+  }
+  for (let i = startPoint; i <= endPoint; i++) {
+      pages.push(i)
+  }
+  if(endPoint !== totalPage){
+      pages.push("...")
+  }
+  return pages
+}
+
 export {
+  pageArray,
   getOffset,
   emptyOrRows
 }
