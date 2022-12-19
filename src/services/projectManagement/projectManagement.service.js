2395966accf5af8d2b0400b79e1a1c011a74c1cc
Wim <policebeam@gmail.com>
2022-12-19 19:04:19 +0700

    Update projectManagement.service.js

--- src/services/projectManagement/projectManagement.service.js
+++ src/services/projectManagement/projectManagement.service.js
@@ -1,25 +1,33 @@
 import admin from 'firebase-admin'
-import { BadRequestError, NotFoundError } from '../../utils/api-errors.js'
+import { BadRequestError} from '../../utils/api-errors.js'
 
 const getWorks = async ({
     page = 1,
     pageSize = 2
 }) => {
-    const offset = pageSize * (page - 1);
-    const db = admin.firestore()
-    const snapshot = await db.collection('works')
-    .limit(pageSize).offset(offset).get()
+    try {
+        const offset = pageSize * (page - 1);
+        const db = admin.firestore()
+        const snapshot = await db.collection('works')
+        .limit(pageSize).offset(offset).get()
+        return snapshot.docs.map((res)=>{
+            return res.data()
+        })
+    } catch (error) {
+        throw new BadRequestError(error.message);
+    }
     
-    return snapshot.docs.map((res)=>{
-        return res.data()
-    })
 
 }
 
 const getAllWorksCount = async () =>{
-    const db = admin.firestore()
-    const result = await db.collection('works').get()
-    return result.size
+    try {
+        const db = admin.firestore()
+        const result = await db.collection('works').get()
+        return result.size
+    } catch (error) {
+        throw new BadRequestError(error.message);
+    }
 }
 
 export {
