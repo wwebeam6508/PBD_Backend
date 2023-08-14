750f9b70577de0fae4c17cc194c48b6caba3fd85
Wim <policebeam@gmail.com>
2023-08-14 22:14:06 +0700

    cache forceString

--- src/utils/caching.js
+++ src/utils/caching.js
@@ -1,6 +1,6 @@
 import NodeCache from "node-cache";
 const cache = new NodeCache();
-
+cache.options.forceString = true;
 export default {
   getFromCache: (key) => cache.get(key),
   setCache: (key, data, ttl) => cache.set(key, data, ttl),
